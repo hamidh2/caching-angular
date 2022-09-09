ee2e32699dd0d4a282aa2b35f788fa21b8128f3a
hamidh2 <hamidhasani94@outlook.com>
2022-09-09 11:37:23 +0430

    :hammer: before caching requests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./modules/user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: 'comments',
+    loadChildren: () =>
+      import('./modules/comment/comment.module').then((m) => m.CommentModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
